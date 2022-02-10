cd8212f19273e4df59bf1a50d9021c8a90726a4c
XabierGM <47028460+XabierGM@users.noreply.github.com>
2022-02-10 14:44:22 +0100

    incompleto

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/app.css">
+    <title>Mi blog</title>
+</head>
+<body>
+    <article>
+    <h1><a href="/post">Mi primera publicación</a></h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi sequi repudiandae blanditiis alias atque, natus nam distinctio numquam facere iure debitis pariatur dicta a optio necessitatibus. Omnis ex explicabo dignissimos.</p>
+    </article>
+
+    <article>
+    <h1><a href="/post">Mi segunda publicación</a></h1>
+    <p>Joróbate mIsa dolor, sit amet consectetur adipisicing elit. Modi sequi repudiandae blanditiis alias atque, natus nam distinctio numquam facere iure debitis pariatur dicta a optio necessitatibus. Omnis ex explicabo dignissimos.</p>
+    </article>
+
+    <article>
+    <h1><a href="/post">Mi tercera publicación</a></h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi sequi repudiandae blanditiis alias atque, natus nam distinctio numquam facere iure debitis pariatur dicta a optio necessitatibus. Omnis ex explicabo dignissimos.</p>
+    </article>
+</body>
+</html>
